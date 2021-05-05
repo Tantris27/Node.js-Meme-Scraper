3a24b687670a78d64fe89bc8c21448575997eb3c
Tristan <tantris.kantner@gmail.com>
2021-05-05 17:32:41 +0200

    Add Dependencies & Update Index

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const fetch = require('node-fetch');
-let html;
+const fs = require('fs');
+const cheerio = require('cheerio');
 
-fetch('https://memegen-link-examples-upleveled.netlify.app/').then((res) => {
-  html = res.text();
-  console.log(html);
-});
+const getReddit = async () => {
+  const response = await fetch(
+    'https://memegen-link-examples-upleveled.netlify.app/',
+  );
+  const body = await response.text();
+
+  const $ = cheerio.load(body);
+  const titleList = [];
+
+  $('img').each((i, title) => {
+    if (i < 10) {
+      titleList.push(title.attribs.src);
+    }
+  });
+  for (let j = 0; j < 10; j++) {
+    const url = titleList[j];
+    async function download() {
+      const response2 = await fetch(url);
+      const buffer = await response2.buffer();
+      fs.writeFile(`./Meme/image${j}.jpg`, buffer, () =>
+        console.log('finished downloading!'),
+      );
+    }
+    download(titleList[j]);
+  }
+};
+
+getReddit();
